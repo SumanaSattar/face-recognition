c16bee7b81248df9f4cdecf0cfaaad5b63bef5b9
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2020-11-19 11:15:27 +0100

    it is detecting the face

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+
 const imgUpload = document.querySelector("#imgUpload");
+
 Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
@@ -6,5 +8,34 @@ Promise.all([
 ]).then(start)
 
 function start() {
-    document.body.append("Loaded");
+    const container = document.createElement('div');
+    container.style.position = 'relative' ;
+    document.body.append(container);
+    console.log(container);
+    console.log(imgUpload);
+
+    imgUpload.addEventListener('change',async () => {
+
+        const image = await faceapi.bufferToImage(imgUpload.files[0]);
+        container.append(image);
+        const canvas = faceapi.createCanvasFromMedia(image);
+        container.append(canvas);
+        const viewSize = { width:image.width,height:image.height };
+        faceapi.matchDimensions(canvas,viewSize);
+
+        const detections = await faceapi.detectAllFaces(image)
+        .withFaceLandmarks().withFaceDescriptors();
+        const resizedDetections = faceapi.resizeResults(detections,viewSize);
+        resizedDetections.forEach( detection => {
+            const box = detection.detection.box
+            const drawBox = new faceapi.draw.DrawBox (box, {label:'Face'})
+            drawBox.draw(canvas);
+        })
+        
+
+    })
+
+    
 }
+
+    
